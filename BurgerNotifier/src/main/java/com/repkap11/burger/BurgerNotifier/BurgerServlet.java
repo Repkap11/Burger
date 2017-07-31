81127221c2e86bb80949b669cbb655dfdd19b258
Paul Repka <repkap11@gmail.com>
2017-07-30 21:40:24 -0400

    Got email notifications to work
    Signed-off-by: Paul Repka <repkap11@gmail.com>

---
+++ BurgerNotifier/src/main/java/com/repkap11/burger/BurgerNotifier/BurgerServlet.java
@@ -0,0 +1,121 @@
+package com.repkap11.burger.BurgerNotifier;
+
+import com.google.api.client.googleapis.auth.clientlogin.ClientLogin;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Iterator;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.*;
+import javax.xml.ws.Response;
+
+public class BurgerServlet extends HttpServlet {
+    static Logger Log = Logger.getLogger("BurgerNotifier");
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        Log.info("Sending the todo list email.");
+
+        String outString;
+        outString = "<p>Sending the todo list email.</p><p><strong>Note:</strong> ";
+        outString = outString.concat("the servlet must be deployed to App Engine in order to ");
+        outString = outString.concat("send the email. Running the server locally writes a message ");
+        outString = outString.concat("to the log file instead of sending an email message.</p>");
+
+        resp.getWriter().println(outString);
+
+        // Note: Ensure that the [PRIVATE_KEY_FILENAME].json has read
+        // permissions set.
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setServiceAccount(getServletContext().getResourceAsStream("/WEB-INF/Burger-42de36b9e316.json"))
+                .setDatabaseUrl("https://burger-1af72.firebaseio.com/")
+                .build();
+
+        try {
+            FirebaseApp.getInstance();
+        } catch (Exception error) {
+            Log.info("doesn't exist...");
+        }
+
+        try {
+            FirebaseApp.initializeApp(options);
+        } catch (Exception error) {
+            Log.info("already exists...");
+        }
+
+        // As an admin, the app has access to read and write all data, regardless of Security Rules
+        DatabaseReference ref = FirebaseDatabase
+                .getInstance()
+                .getReference("todoItems");
+
+        // This fires when the servlet first runs, returning all the existing values
+        // only runs once, until the servlet starts up again.
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Object document = dataSnapshot.getValue();
+                Log.info("new value: " + document);
+
+                String todoText = "Don't forget to...\n\n";
+
+                Iterator<DataSnapshot> children = dataSnapshot.getChildren().iterator();
+
+                while (children.hasNext()) {
+                    DataSnapshot childSnapshot = (DataSnapshot) children.next();
+                    todoText = todoText + " * " + childSnapshot.getValue().toString() + "\n";
+                }
+
+                // Now send the email
+
+                // Note: When an application running in the development server calls the Mail
+                // service to send an email message, the message is printed to the log.
+                // The Java development server does not send the email message.
+
+                // You can test the email without waiting for the cron job to run by
+                // loading http://[FIREBASE_PROJECT_ID].appspot.com/send-email in your browser.
+
+                Properties props = new Properties();
+                Session session = Session.getDefaultInstance(props, null);
+                try {
+                    Message msg = new MimeMessage(session);
+                    //Make sure you substitute your project-id in the email From field
+                    msg.setFrom(new InternetAddress("reminder@burger-1af72.appspotmail.com",
+                            "Todo Reminder"));
+                    msg.addRecipient(Message.RecipientType.TO,
+                            new InternetAddress("repkap11@gmail.com", "Recipient"));
+                    msg.setSubject("Things to do today");
+                    msg.setText(todoText);
+                    Transport.send(msg);
+                } catch (MessagingException | UnsupportedEncodingException e) {
+                    Log.warning(e.getMessage());
+                }
+
+                // Note: in a production application you should replace the hard-coded email address
+                // above with code that populates msg.addRecipient with the app user's email address.
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                System.out.println("Error: " + error);
+            }
+        });
+    }
+}
