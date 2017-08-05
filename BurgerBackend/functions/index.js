b9613553835f60e0e4096cace2e82d0e2e859e96
Paul Repka <repkap11@gmail.com>
2017-08-04 20:26:06 -0400

    Get events with database values change
    Signed-off-by: Paul Repka <repkap11@gmail.com>

--- BurgerBackend/functions/index.js
+++ BurgerBackend/functions/index.js
@@ -5,12 +5,25 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-
-
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello From Burger Firebase hosted web service!");
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+exports.addMessage = functions.https.onRequest((req, res) => {
+    // Grab the text parameter.
+    const original = req.query.text;
+    // Push the new message into the Realtime Database using the Firebase Admin SDK.
+    admin.database().ref('/messages').push("Test value");
 });
+
+exports.group_users_with_locations = functions.database.ref('/users/{userId}/lunch_preference_1')
+    .onWrite(event => {return group_users_with_locations(event, 1)});
+
+function group_users_with_locations(event, pref_number){
+    // Grab the current value of what was written to the Realtime Database.
+    console.log('You changed ',event.params.userId, '\'s lunch pref ', event.data.ref.key, pref_number, ' lunch to ', event.data.val());
+    //event.data.ref
+    // You must return a Promise when performing asynchronous tasks inside a Functions such as
+    // writing to the Firebase Realtime Database.
+    // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+    //return event.data.ref.parent.child('uppercase').set(uppercase);
+    return null;
+}
