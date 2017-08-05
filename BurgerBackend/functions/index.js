3aa768d781bce5ab4627a9c86a1c8dee2727fdb7
Paul Repka <repkap11@gmail.com>
2017-08-04 22:39:53 -0400

    Users list of location preferences is properly cleaned up when they remove that location
    Signed-off-by: Paul Repka <repkap11@gmail.com>

--- BurgerBackend/functions/index.js
+++ BurgerBackend/functions/index.js
@@ -16,16 +16,25 @@ exports.add_user_to_locations_pref4 = functions.database.ref('/users/{userId}/lu
 exports.add_user_to_locations_pref5 = functions.database.ref('/users/{userId}/lunch_preference_5')
     .onWrite(event => {return add_user_to_locations_pref(event)});
 
+//event is Event (in our case a DeltaSnapshot type event
+//event.data is DeltaSnapshot
+
 function add_user_to_locations_pref(event){
-    // Grab the current value of what was written to the Realtime Database.
-    console.log('You changed ',event.params.userId, '\'s lunch pref ', event.data.ref.key, ' lunch to ', event.data.val());
-    var lunch_location_ref = event.data.ref.parent.parent.parent.child("lunch_locations").child(event.data.val());
-    lunch_location_ref.child(event.data.ref.key).child("users").push(event.params.userId);
+    //When a user changes their preference for any day, we get a message here
+    //console.log('You changed ',event.params.userId, '\'s lunch pref ', event.data.ref.key, ' from ', event.data.previous.val(),' to ', event.data.current.val());
+     //Figure out which lunch preference they are no longer picking for that day.
+
+     var val_previous = event.data.previous.val();
+     if (val_previous != null){
+        //Remove the users id from that location's list of users for that day
+        var lunch_location_previous_ref = event.data.ref.parent.parent.parent.child("lunch_locations").child(val_previous);
+        lunch_location_previous_ref.child(event.data.ref.key).child('users').child(event.params.userId).remove();
+     }
+      var val_current = event.data.current.val();
+     if (val_current != null){
+        //Add the users id to that location's list of users for that day
+        var lunch_location_current_ref = event.data.ref.parent.parent.parent.child("lunch_locations").child(val_current);
+        lunch_location_current_ref. child(event.data.ref.key).child('users').child(event.params.userId).set('')
+    }
     return null;
 }
-
-//    lunch_location_ref.once('value').then(function(dataSnapshot) {
-//        // handle read data.
-//        console.log('You changed lunch pref ', event.data.ref.key, pref_number, ' lunch to ', dataSnapshot.val().displayName);
-//
-//     });
