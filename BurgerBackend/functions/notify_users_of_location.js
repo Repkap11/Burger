c63fe7dac886c8d1b781d7de99104d6bdcc2ee7e
Paul Repka <repkap11@gmail.com>
2017-08-06 21:25:52 -0400

    Add better notifications
    Signed-off-by: Paul Repka <repkap11@gmail.com>

--- BurgerBackend/functions/notify_users_of_location.js
+++ BurgerBackend/functions/notify_users_of_location.js
@@ -6,38 +6,42 @@ const admin = require('firebase-admin');
 
 exports.send_notification_for_date = function send_notification_for_date(req, res){
     console.error('You called this function');
-    var lunchGroup = admin.database().ref('lunch_groups').child('-KqqZZp2_qIcTsvDusBx');
-    var groupsLocations = lunchGroup.child('lunch_locations');
-    groupsLocations.once('value').then(function(lunchLocationsSnapshot) {
+    var lunchGroups = admin.database().ref('lunch_groups');
+    lunchGroups.once('value').then(function(lunchGroupsSnapshot){
 
-        //console.error('Read Lunch Locations:', lunchLocationsSnapshot.key,' val:', lunchLocationsSnapshot.val());
-        lunchLocationsSnapshot.forEach(function(locationSnapshot) {
+        lunchGroupsSnapshot.forEach(function(lunchGroupSnapshot){
 
-            //console.error('Read Location:', locationSnapshot.key,' val:', locationSnapshot.val());
-            var locationName = locationSnapshot.child('displayName').val();
-            locationSnapshot.child('lunch_preference_1').child('users').forEach(function(userKeySnapshot){
+           console.error('Read Lunch Group:'+ lunchGroupSnapshot.key+' val:'+ lunchGroupSnapshot.val());
+           var lunchGroupName = lunchGroupSnapshot.child('displayName').val();
+           lunchGroupSnapshot.child('lunch_locations').forEach(function(lunchLocationSnapshot) {
 
-                //console.error('Read UserKey:', userKeySnapshot.key,' val:', userKeySnapshot.val());
-                lunchGroup.child('users').child(userKeySnapshot.key).once('value').then(function(userSnapshot){
+                console.error('Read Location:'+ lunchLocationSnapshot.key+' val:'+ lunchLocationSnapshot.val());
+                var locationName = lunchLocationSnapshot.child('displayName').val();
+                lunchLocationSnapshot.child('lunch_preference_1').child('users').forEach(function(userKeySnapshot){
 
-                    //console.error('Read ReadUser:', userSnapshot.key,' val:', userSnapshot.val());
+                    console.error('Read UserKey:'+ userKeySnapshot.key+' val:'+ userKeySnapshot.val());
+                    var userSnapshot = lunchGroupSnapshot.child('users').child(userKeySnapshot.key);
+
+                    console.error('Read ReadUser:'+ userSnapshot.key+' val:'+ userSnapshot.val());
                     var userName = userSnapshot.child('displayName').val();
                     userSnapshot.child('devices').forEach(function(userDeviceSnapshot){
-                        console.error('Will notify device:'+ userDeviceSnapshot.key,' val:'+ userDeviceSnapshot.val());
-                        var title = 'Lunch Reminder';
+                        console.error('Will notify device:'+ userDeviceSnapshot.key);
+                        var title = 'Lunch Reminder (' +lunchGroupName+')';
                         var body = userName+', you have lunch at '+locationName+' today'
                          const payload = {
-                              notification: {
+                              data: {
                                 title: title,
-                                body: body
+                                body: body,
                               }
                         }
-                        console.error('Notifying:',userName,' of lunch at ', locationName);
+                        console.error('Notifying:'+userName+' of lunch at '+ locationName);
                         admin.messaging().sendToDevice(userDeviceSnapshot.key, payload);
                     });
                 });
+
+
             });
         });
-    });
+     });
     res.status(200).send("Triggering notifications");
 }
---
+++ app/src/main/java/com/repkap11/burger/BurgerNotificationService.java
@@ -0,0 +1,64 @@
+package com.repkap11.burger;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.repkap11.burger.activities.SignInFractivity;
+
+import java.util.Map;
+
+public class BurgerNotificationService extends FirebaseMessagingService {
+
+    private static final String TAG = BurgerNotificationService.class.getSimpleName();
+
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Map<String, String> map = remoteMessage.getData();
+        sendNotification(map.get("title"), map.get("body"));
+    }
+
+    private void sendNotification(String title, String body) {
+        Log.e(TAG, "Notification Message Title: " + title + " body:" + body);
+
+        Intent intent = new Intent(this, SignInFractivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.burger_notification_icon)
+                .setContentTitle(title == null ? getResources().getString(R.string.app_name) : title)
+                //.setSubText(body)
+                .setContentText(body)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setVibrate(new long[]{0, 300, 300, 300})
+                .setLights(Color.RED, 1000, 1000)
+                .setContentIntent(pendingIntent);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            notificationBuilder.setColor(getResources().getColor(R.color.colorAccent, getTheme()));
+        } else {
+            notificationBuilder.setColor(getResources().getColor(R.color.colorAccent));
+        }
+        //
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+
+
+}
