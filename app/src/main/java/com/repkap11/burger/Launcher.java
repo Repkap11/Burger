0ff7ffd6228dd7d59ea765f1f317f9c898b8ed98
Paul Repka <repkap11@gmail.com>
2017-09-24 14:25:12 -0400

    Don't use activity alias, since they don't restart correctly
    Signed-off-by: Paul Repka <repkap11@gmail.com>

---
+++ app/src/main/java/com/repkap11/burger/Launcher.java
@@ -0,0 +1,8 @@
+package com.repkap11.burger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.repkap11.burger.activities.SignInFractivity;
+
+public class Launcher extends SignInFractivity {}
