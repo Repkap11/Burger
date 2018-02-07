5e8a995562fa0a35ed3fe56b412d41cfb6d5602c
Paul Repka <repkap11@gmail.com>
2018-02-06 23:45:32 -0500

    Update to version 14, and leave the same message since it still applies
    Signed-off-by: Paul Repka <repkap11@gmail.com>

--- app/upload-apk.sh
+++ app/upload-apk.sh
@@ -5,8 +5,9 @@ if [ "$#" -ne 1 ]; then
 	exit
 fi
 APP_VERSION=$1
-LOCAL_FILE=./burger-live-debug-$APP_VERSION.apk
+LOCAL_FILE=./build/outputs/apk/live/debug/app-live-debug-$APP_VERSION.apk
 
+cp $LOCAL_FILE ./burger-live-debug-$APP_VERSION.apk
 REMOTE_USERNAME=paul
 REMOTE_SERVER=repkap11.com
 REMOTE_FILE=/home/paul/website/burger/Burger.apk
@@ -14,4 +15,4 @@ REMOTE_TARGET=$REMOTE_USERNAME@$REMOTE_SERVER:$REMOTE_FILE
 echo "Copying $LOCAL_FILE to $REMOTE_TARGET"
 rsync --update $LOCAL_FILE $REMOTE_TARGET
 firebase --project burger-1af72 database:set -y -d $APP_VERSION /lunch_groups/appVersion
-
+echo "Upload Complete"
