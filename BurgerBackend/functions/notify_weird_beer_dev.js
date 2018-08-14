d0528bbcd337340af4ef8865c8b44c96870df30a
Paul Repka <repkap11@gmail.com>
2018-08-13 21:55:53 -0400

    Format JS files
    Signed-off-by: Paul Repka <repkap11@gmail.com>

--- BurgerBackend/functions/notify_weird_beer_dev.js
+++ BurgerBackend/functions/notify_weird_beer_dev.js
@@ -1,37 +1,36 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 
-exports.send_notification_for_beer = function send_notification_for_beer(event, root_key_lunch_groups){
-    console.error('You called send_notification_for_beer');
-    var lunchGroups = admin.database().ref(root_key_lunch_groups);
-    return lunchGroups.once('value').then(function(lunchGroupsSnapshot){
-        var allBeedNotifications = [];
-        lunchGroupsSnapshot.forEach(function(lunchGroupSnapshot){
-
-            var groupName = lunchGroupSnapshot.child('displayName').val();
-            var groupHasWeirdBeer = lunchGroupSnapshot.child('hasWeirdBeer').val();
-            //console.error('Read Lunch Group:'+ groupName+' hasWeirdBeer:'+groupHasWeirdBeer);
-            if (groupHasWeirdBeer){
-                lunchGroupSnapshot.child('users').forEach(function(userSnapshot){
-                    userSnapshot.child('devices').forEach(function(userDeviceSnapshot){
-                    var userName = userSnapshot.child('displayName').val();
-                    var title = 'Weird Beer Reminder (' +groupName+')';
-                    var body = 'Weird Beer is starting soon. Don\'t miss out!';
-                     const payload = {
-                          data: {
-                            title: title,
-                            body: body,
-                          }
-                    }
-                    console.log('Notifying:'+userName+' of weird beer');
-                    allBeedNotifications.push(admin.messaging().sendToDevice(userDeviceSnapshot.key, payload));
-                });
-            });
-            }
+exports.send_notification_for_beer = function send_notification_for_beer(event, root_key_lunch_groups) {
+  console.error("You called send_notification_for_beer");
+  var lunchGroups = admin.database().ref(root_key_lunch_groups);
+  return lunchGroups.once("value").then(function(lunchGroupsSnapshot) {
+    var allBeedNotifications = [];
+    lunchGroupsSnapshot.forEach(function(lunchGroupSnapshot) {
+      var groupName = lunchGroupSnapshot.child("displayName").val();
+      var groupHasWeirdBeer = lunchGroupSnapshot.child("hasWeirdBeer").val();
+      //console.error('Read Lunch Group:'+ groupName+' hasWeirdBeer:'+groupHasWeirdBeer);
+      if (groupHasWeirdBeer) {
+        lunchGroupSnapshot.child("users").forEach(function(userSnapshot) {
+          userSnapshot.child("devices").forEach(function(userDeviceSnapshot) {
+            var userName = userSnapshot.child("displayName").val();
+            var title = "Weird Beer Reminder (" + groupName + ")";
+            var body = "Weird Beer is starting soon. Don't miss out!";
+            const payload = {
+              data: {
+                title: title,
+                body: body
+              }
+            };
+            console.log("Notifying:" + userName + " of weird beer");
+            allBeedNotifications.push(admin.messaging().sendToDevice(userDeviceSnapshot.key, payload));
+          });
         });
-        return Promise.all(allBeedNotifications)
+      }
     });
-}
+    return Promise.all(allBeedNotifications);
+  });
+};
